f38f22cf8261799afb096f0bcba567fb0d1d3e17
Tor Nilsson <bas12tni@student.lu.se>
2016-02-10 16:17:12 +0100

    Added more methods!

--- src/Journal.java
+++ src/Journal.java
@@ -3,6 +3,50 @@ public class Journal {
 	private String Patient;
 	private String Doctor;
 	private String Nurse;
-	private String Journal;
+	private String Division;
 	private String Data;
+
+	public Journal() {
+
+	}
+
+	public void setPatient(String Patient) {
+		Patient = Patient;
+	}
+
+//	public void setDoctor() {
+//		return Data;
+//	}
+//
+//	public void setNurse() {
+//		return Data;
+//	}
+//
+//	public void setDivision() {
+//		return Data;
+//	}
+//
+//	public void setData() {
+//		return Data;
+//	}
+	
+	public String setPatient() {
+		return Data;
+	}
+
+	public String getDoctor() {
+		return Data;
+	}
+
+	public String getNurse() {
+		return Data;
+	}
+
+	public String getDivision() {
+		return Data;
+	}
+
+	public String getData() {
+		return Data;
+	}
 }
