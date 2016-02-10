f912d9bd8f1c6c2a3fc4054810da346a3a497aff
zionzept <dat13tha@student.lu.se>
2016-02-10 16:53:05 +0100

    Logging added. Use logger.info("txt") in Server!

--- src/server/Server.java
+++ src/server/Server.java
@@ -1,13 +1,32 @@
 package server;
-import java.io.*;
-import java.net.*;
-import java.security.KeyStore;
-import javax.net.*;
-import javax.net.ssl.*;
-import javax.security.cert.X509Certificate;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.math.BigInteger;
+import java.net.ServerSocket;
+import java.security.KeyStore;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+import javax.net.ServerSocketFactory;
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.TrustManagerFactory;
+import javax.security.cert.X509Certificate;
 
 public class Server implements Runnable {
+	private static final String logPath = "H:\\";
+	private static final Logger logger = Logger.getLogger( Server.class.getName() );
+	static FileHandler fh;
     private ServerSocket serverSocket = null;
     private static int numConnectedClients = 0;
 
@@ -15,7 +34,7 @@ public class Server implements Runnable {
         serverSocket = ss;
         newListener();
     }
-
+    
     public void run() {
         try {
             SSLSocket socket=(SSLSocket)serverSocket.accept();
@@ -62,7 +81,7 @@ public class Server implements Runnable {
     private void newListener() { (new Thread(this)).start(); } // calls run()
 
     public static void main(String args[]) {
-        System.out.println("\nServer Started\n");
+        setupLogger();
         int port = -1;
         if (args.length >= 1) {
             port = Integer.parseInt(args[0]);
@@ -77,6 +96,7 @@ public class Server implements Runnable {
             System.out.println("Unable to start Server: " + e.getMessage());
             e.printStackTrace();
         }
+    	
     }
 
     private static ServerSocketFactory getServerSocketFactory(String type) {
@@ -104,5 +124,19 @@ public class Server implements Runnable {
             return ServerSocketFactory.getDefault();
         }
         return null;
+    }
+    
+    // Sets up logger to write to file. A new file will be made for each day.
+    private static void setupLogger() {
+	        SimpleDateFormat format = new SimpleDateFormat("MM-dd");
+	        try {new FileHandler();   
+	            fh = new FileHandler(logPath + "Server_"
+	                + format.format(Calendar.getInstance().getTime()) + ".log", true);
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	        fh.setFormatter(new SimpleFormatter());
+	        logger.addHandler(fh);
+	        logger.info("startup");
     }
 }
